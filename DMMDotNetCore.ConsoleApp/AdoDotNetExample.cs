86fa4541f3ce26b3bcd4f7283640e2b20a998e11
David Mg Mg <jackjack131217@gmail.com>
2024-11-08 18:34:46 +0630

    delete some and create some lol idk anymore

--- DMMDotNetCore.ConsoleApp/AdoDotNetExample.cs
+++ DMMDotNetCore.ConsoleApp/AdoDotNetExample.cs
@@ -22,14 +22,53 @@ namespace DMMDotNetCore.ConsoleApp
         {
             SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
             connection.Open();
-            Console.WriteLine("Connection Open");
+           
             string query = "select * from Tbl_Blog";
             SqlCommand command = new SqlCommand(query, connection);
             SqlDataAdapter adapter = new SqlDataAdapter(command);
             DataTable dt = new DataTable();
             adapter.Fill(dt);
             connection.Close();
-            Console.WriteLine("Connection Close");
+            
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                Console.WriteLine("Blog Id => " + dr["BlogId"]);
+                Console.WriteLine("Blog Title => " + dr["BlogTitle"]);
+                Console.WriteLine("Blog Author => " + dr["BlogAuthor"]);
+                Console.WriteLine("Blog Content => " + dr["BlogContent"]);
+                Console.WriteLine("#####################");
+            }
+        }
+
+        public void Create(string title, string author, string content)
+        {
+            SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
+
+            connection.Open();
+
+            string query = @"INSERT INTO [dbo].[Tbl_Blog]
+           ([BlogTitle]
+           ,[BlogAuthor]
+           ,[BlogContent])
+     VALUES
+           (@BlogTitle
+           , @BlogAuthor
+           , @BlogContent)";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@BlogTitle", title);
+            cmd.Parameters.AddWithValue("@BlogAuthor", author);
+            cmd.Parameters.AddWithValue("@BlogContent", content);
+            int result = cmd.ExecuteNonQuery();
+            connection.Close();
+
+            string message = result > 0 ? "Saving Successful." : "Saving Failed.";
+            Console.WriteLine(message);
+        }
+
+        public void Update(int id, string title, string author, string content)
+        {
+
         }
 
     }
