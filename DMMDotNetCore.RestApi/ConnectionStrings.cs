236e73e292b41a9929d2c37fd9614e8d87a109cd
David Mg Mg <jackjack131217@gmail.com>
2024-12-26 16:36:37 +0630

    rest api

---
+++ DMMDotNetCore.RestApi/ConnectionStrings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMMDotNetCore.RestApi
+{
+    internal class ConnectionStrings
+    {
+        public static SqlConnectionStringBuilder SqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = "localhost\\SQLEXPRESS",
+            InitialCatalog = "DotNetTraining",
+            UserID = "sa",
+            Password = "pass",
+            TrustServerCertificate = true
+        };
+    }
+}
--- DMMDotNetCore.RestApi/Controllers/BlogController.cs
+++ DMMDotNetCore.RestApi/Controllers/BlogController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DMMDotNetCore.RestApi.Database;
+using DMMDotNetCore.RestApi.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata;
 
 namespace DMMDotNetCore.RestApi.Controllers
 {
@@ -7,39 +10,105 @@ namespace DMMDotNetCore.RestApi.Controllers
     [ApiController]
     public class BlogController : ControllerBase
     {
+        private readonly AppDbContext _context;
+
+        public BlogController()
+        {
+            _context = new AppDbContext();
+        }
+
         [HttpGet]
         public IActionResult Read()
         {
+            var lst = _context.Blogs.ToList();
+            return Ok(lst);
+        }
 
-            return Ok();
+        [HttpGet("{id}")]
+        public IActionResult Edit(int id)
+        {
+            var item = _context.Blogs.FirstOrDefault(x => x.BlogId == id);
+
+            if (item is null)
+            {
+                return NotFound("No data found.");
+            }
+
+            return Ok(item);
         }
 
         [HttpPost]
-        public IActionResult Create()
+        public IActionResult Create(BlogModel blog)
         {
+            _context.Blogs.Add(blog);
+            var result = _context.SaveChanges();
 
-            return Ok();
+            string message = result > 0 ? "Saving successful" : "Saving Failed";
+            return Ok(message);
+        } 
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, BlogModel blog)
+        {
+            var item = _context.Blogs.FirstOrDefault(x => x.BlogId == id);
+
+            if ( item is null)
+            {
+                return NotFound("No data found");
+            }
+
+            item.BlogTitle = blog.BlogTitle;
+            item.BlogAuthor = blog.BlogAuthor;
+            item.BlogContent = blog.BlogContent;
+            var result = _context.SaveChanges();
+
+            string message = result > 0 ? "Saving successful" : "Saving Failed";
+            return Ok(message);
+
         }
 
-        [HttpPut]
-        public IActionResult Update()
+        [HttpPatch("{id}")]
+        public IActionResult Patch(int id, BlogModel blog)
         {
+            var item = _context.Blogs.FirstOrDefault(x => x.BlogId == id);
 
-            return Ok();
-        }
+            if (item is null)
+            {
+                return NotFound("No data found");
+            }
+            if (!string.IsNullOrEmpty(blog.BlogTitle))
+            {
+                item.BlogTitle = blog.BlogTitle;
+            }
+            if (!string.IsNullOrEmpty(blog.BlogAuthor))
+            {
+                item.BlogAuthor = blog.BlogAuthor;
+            }
+               
+            if (!string.IsNullOrEmpty(blog.BlogContent))
+            {
+                item.BlogContent = blog.BlogContent;
+            }
+                
+            var result = _context.SaveChanges();
 
-        [HttpPatch]
-        public IActionResult Patch()
-        {
-
-            return Ok();
+            string message = result > 0 ? "Saving successful" : "Saving Failed";
+            return Ok(message);
         }
 
         [HttpDelete]
-        public IActionResult Delete()
+        public IActionResult Delete(int id)
         {
+            var item = _context.Blogs.FirstOrDefault(x => x.BlogId == id);
+            if(item is null)
+            {
+                return NotFound("No data found");
+            }
+            _context.Blogs.Remove(item);
+            var result = _context.SaveChanges();
 
-            return Ok();
+            string message = result > 0 ? "Delete successful" : "failed";
+            return Ok(message);
         }
     }
 }
