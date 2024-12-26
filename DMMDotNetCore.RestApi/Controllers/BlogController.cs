4ff8da1d1071f55e70e4ca112945bcf6f55da046
David Mg Mg <jackjack131217@gmail.com>
2024-12-26 11:57:10 +0630

    testing

---
+++ DMMDotNetCore.RestApi/Controllers/BlogController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DMMDotNetCore.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Read()
+        {
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Create()
+        {
+
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Update()
+        {
+
+            return Ok();
+        }
+
+        [HttpPatch]
+        public IActionResult Patch()
+        {
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+
+            return Ok();
+        }
+    }
+}
